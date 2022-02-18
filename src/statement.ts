567eec571780a74e4cb87937b8b5a710cd8ff965
ThievingSix <contact@dokkan.wiki>
2022-02-17 18:09:07 -0800

    Statement class that eval()s its conditional code in an Executor instance

---
+++ src/statement.ts
@@ -0,0 +1,55 @@
+import {Executor} from './executor';
+import {Optional} from './utils';
+
+export enum StatementType {
+    define, if, else, elif, ifdef, ifndef, elifdef, elifndef, endif
+}
+
+export const STATEMENT_TEST_TYPES = [StatementType.ifdef, StatementType.ifndef, StatementType.elifdef, StatementType.elifndef];
+export const STATEMENT_TEST_NEGATE_TYPES = [StatementType.ifndef, StatementType.elifndef];
+
+export interface StatementLocation {
+    filename?: string,
+    line?: number,
+    column?: number
+}
+
+export class Statement {
+    type: StatementType
+    params: string[]
+    location?: StatementLocation
+    value: Optional<boolean>
+
+    constructor(type: StatementType, params: string[], location?: StatementLocation) {
+        this.type = type
+        this.params = params
+        this.location = location
+    }
+
+    async execute(executor: Executor): Promise<Optional<boolean>> {
+        if (this.type === StatementType.define) {
+            await executor.addDefine(
+                this.params[0],
+                this.params.length === 1 ? true : JSON.parse(this.params[1]),
+            );
+            this.value = undefined;
+        } else if (STATEMENT_TEST_TYPES.includes(this.type)) {
+            const code = `typeof ${this.params[0]} !== 'undefined'`;
+            let result = !!await executor.exec(code);
+            if (STATEMENT_TEST_NEGATE_TYPES.includes(this.type)) {
+                result = !result;
+            }
+            this.value = result;
+        } else {
+            this.value = !!await executor.exec(
+                this.params[0],
+                this.location?.filename,
+                this.location?.line,
+                this.location?.column,
+            );
+        }
+        return this.value;
+    }
+
+}
+
