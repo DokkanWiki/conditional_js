9853cad5deaa121c0a650c58ac57ec0d5c730a57
ThievingSix <contact@dokkan.wiki>
2022-03-04 13:01:36 -0800

    Tests for statement_parser

---
+++ src/statement_parser.test.ts
@@ -0,0 +1,32 @@
+import {StatementLocation, StatementType} from './statement';
+import {DEFAULT_OPTIONS} from './defaults';
+import {normalizeOptions} from './options';
+import {StatementParser} from './statement_parser';
+
+const default_options = normalizeOptions(DEFAULT_OPTIONS);
+
+describe.each([
+    ['@define identifier', StatementType.define, ['identifier']],
+    ['@if identifier', StatementType.if, ['identifier']],
+    ['@ifdef identifier', StatementType.ifdef, ['identifier']],
+    ['@ifndef identifier', StatementType.ifndef, ['identifier']],
+    ['@else', StatementType.else, []],
+    ['@elif identifier', StatementType.elif, ['identifier']],
+    ['@elifdef identifier', StatementType.elifdef, ['identifier']],
+    ['@elifndef identifier', StatementType.elifndef, ['identifier']],
+    ['@endif', StatementType.endif, []],
+])('%s', (line, result_type, result_params) => {
+    test(`match [${result_type}, ${JSON.stringify(result_params)}`, async () => {
+        const parser = new StatementParser(default_options.parser);
+        const location: StatementLocation = {
+            filename: 'test.js',
+            line: Math.ceil(Math.random() * 100),
+            column: Math.ceil(Math.random() * 100),
+        };
+        expect(parser.parseString(line, location)).toEqual({
+            type: result_type,
+            params: result_params,
+            location: location,
+        });
+    });
+});
