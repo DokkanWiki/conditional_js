69240949f23fb2033438c55757b897c009d2ecf2
ThievingSix <contact@dokkan.wiki>
2022-04-13 15:51:54 -0700

    Version bump to 0.0.3

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ import {ConditionalJsLoader} from './loader';
 
 export default ConditionalJsLoader;
 export {ConditionalJsProcessor} from './processor';
+
+/**
+ * TODO: Have a way of printing all used definitions
+ * TODO: #ifdef on object properties
+ */
